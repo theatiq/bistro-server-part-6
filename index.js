18664a9cb1d4bee1f4c09d56c49485b16b05c1c4
theatiq <theatiq@gmail.com>
2025-01-10 06:11:35 +0600

    Part 7 done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const jwt = require("jsonwebtoken")
 const cors = require('cors');
 require("dotenv").config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
 
 
@@ -36,6 +37,7 @@ async function run() {
         const menuCollection = client.db("bistroDB").collection("menu")
         const reviewsCollection = client.db("bistroDB").collection("reviews")
         const cartsCollection = client.db("bistroDB").collection("carts")
+        const paymentCollection = client.db("bistroDB").collection("payments")
 
         // JWT Related APIs
         app.post("/jwt", (req, res) => {
@@ -193,6 +195,45 @@ async function run() {
             res.send(result)
         })
 
+        // Client Intents
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body
+            const amount = parseInt(price * 100)
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"]
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
+
+        app.get("/payments/:email", verifyToken, async (req, res) => {
+            const query = { email: req.params.email }
+            if(req.params.email !== req.decoded.email){
+                return res.status(401).send({message: "Forbidden Access"})
+            }
+            const result = await paymentCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post("/payment", async (req, res) => {
+            const payment = req.body
+            const paymentResult = await paymentCollection.insertOne(payment)
+
+
+            console.log("Payment Info", payment)
+            const query = {
+                _id: {
+                    $in: payment.cartIds.map(id => new ObjectId(id))
+                }
+            }
+            const deleteResult = await cartsCollection.deleteMany(query)
+            res.send({ paymentResult, deleteResult })
+
+        })
 
 
 
